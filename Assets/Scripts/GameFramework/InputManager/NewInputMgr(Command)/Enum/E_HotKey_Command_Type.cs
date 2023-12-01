27f4bd4aeb132f8eab396ca3f992009d315f7d31
虫儿努力向上飞 <55835505+AzureBubble@users.noreply.github.com>
2023-12-01 23:14:16 +0800

    Clear Up NewInputMgr

--- Assets/Scripts/GameFramework/InputManager/NewInputMgr(Command)/Enum/E_HotKey_Command_Type.cs
+++ Assets/Scripts/GameFramework/InputManager/NewInputMgr(Command)/Enum/E_HotKey_Command_Type.cs
@@ -1,26 +1,5 @@
 /// <summary>
-/// 按键的三种状态枚举
-/// </summary>
-public enum E_KeyCode_Command_Type
-{
-    /// <summary>
-    /// 按键按下
-    /// </summary>
-    Down,
-
-    /// <summary>
-    /// 按键按住
-    /// </summary>
-    Stay,
-
-    /// <summary>
-    /// 按键抬起
-    /// </summary>
-    Up,
-}
-
-/// <summary>
-/// 按键的三种状态枚举
+/// Unity热键枚举
 /// </summary>
 public enum E_HotKey_Command_Type
 {
---
+++ Assets/Scripts/GameFramework/InputManager/NewInputMgr(Command)/Enum/E_KeyCode_Command_Type.cs
@@ -0,0 +1,20 @@
+/// <summary>
+/// 按键的三种状态枚举
+/// </summary>
+public enum E_KeyCode_Command_Type
+{
+    /// <summary>
+    /// 按键按下
+    /// </summary>
+    Down,
+
+    /// <summary>
+    /// 按键按住
+    /// </summary>
+    Stay,
+
+    /// <summary>
+    /// 按键抬起
+    /// </summary>
+    Up,
+}
