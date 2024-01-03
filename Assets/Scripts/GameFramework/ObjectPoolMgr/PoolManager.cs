be8389fd9b4f24b16eacaa804cdf3b2ac920e436
虫儿努力向上飞 <55835505+AzureBubble@users.noreply.github.com>
2024-01-03 10:50:35 +0800

    Update ObjectPool

--- Assets/Scripts/GameFramework/ObjectPoolMgr/PoolManager.cs
+++ Assets/Scripts/GameFramework/ObjectPoolMgr/PoolManager.cs
@@ -8,30 +8,33 @@ namespace GameFramework.ObjectPoolManager
     /// <summary>
     /// Unity 的 ObjectPool 对象池管理器
     /// </summary>
-    public class PoolManager : SingletonAutoMono<PoolManager>
+    public class PoolManager : Singleton<PoolManager>
     {
         /// <summary>
         /// 对象池总容器 键：某一类物品名字，值：对应该类物体的对象池容器
         /// </summary>
         private Dictionary<string, ObjectPool<GameObject>> poolDic = new Dictionary<string, ObjectPool<GameObject>>();
 
+        private List<GameObject> parentObj = new List<GameObject>();
+
         /// <summary>
         /// 创建对象池
         /// </summary>
         /// <param name="objName">游戏物体名字</param>
-        public void CreatePool(string objName, string path = "Prefabs/")
+        private void CreatePool(string objName, string path = "Prefabs/")
         {
             // 缓存池游戏对象
             Transform parent = new GameObject(objName + " Pool").transform;
+            parentObj.Add(parent.gameObject);
             // 设置为缓存池管理器子对象
-            parent.SetParent(this.transform, false);
+            //parent.SetParent(this.transform, false);
             ObjectPool<GameObject> newPool = new ObjectPool<GameObject>(
                 // 游戏对象创建时候的方法
                 () =>
                             {
                                 GameObject obj = null;
                                 obj = ResourcesMgr.Instance.LoadRes<GameObject>(path + objName, objName);
-                                obj.transform.SetParent(parent, false);
+                                obj.transform.SetParent(null, false);
                                 return obj;
                             },
       e =>
@@ -39,18 +42,20 @@ namespace GameFramework.ObjectPoolManager
                                 // 取对象时候的方法
                                 // e:对象池中的每一个元素
                                 e.SetActive(true);
+                                e.transform.SetParent(null, false);
                             },
       e =>
                             {
                                 // 对象放回对象池时候的方法
                                 // e:对象池中的每一个元素
                                 e.SetActive(false);
+                                e.transform.SetParent(parent, false);
                             },
     e =>
                             {
                                 // 对象销毁时候的方法
                                 // e:对象池中的每一个元素
-                                Destroy(e.gameObject);
+                                GameObject.Destroy(e.gameObject);
                             }); // 是否自动扩展， 默认容量：10，最大容量：20
 
             // 把对象池加入到List中
@@ -84,6 +89,27 @@ namespace GameFramework.ObjectPoolManager
             {
                 poolDic[objName].Release(obj);
             }
+        }
+
+        public void Clear()
+        {
+            foreach (var pool in poolDic.Values)
+            {
+                pool.Clear();
+            }
+            poolDic.Clear();
+            foreach (var obj in parentObj)
+            {
+                GameObject.Destroy(obj);
+            }
+            parentObj.Clear();
+        }
+
+        public override void Dispose()
+        {
+            if (IsDisposed) return;
+            Clear();
+            base.Dispose();
         }
     }
 }
--- Assets/Scripts/GameFramework/ObjectPoolMgr/PoolMgr.cs
+++ Assets/Scripts/GameFramework/ObjectPoolMgr/PoolMgr.cs
@@ -1,5 +1,4 @@
 using GameFramework.PackageMgr.ResourcesManager;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -11,74 +10,87 @@ namespace GameFramework.ObjectPoolManager
     /// </summary>
     public class PoolData
     {
-        public GameObject parentObj; // 缓存池结点
-        public List<GameObject> poolList; // 缓存池容器
-        public readonly float clearUpInterval = 300.0f; // 缓存池清理间隔时间 默认时间300秒
-        private float lastUseTime = 0.0f; // 缓存池最后一次使用时间
+        private GameObject parentObj; // 缓存池结点
+        private Stack<GameObject> dataStack = new Stack<GameObject>(); // 没有使用的对象池
+        private List<GameObject> usedList = new List<GameObject>(); // 使用中的对象池
+        public int Count => dataStack.Count;
+        public int UseCount => usedList.Count;
 
         /// <summary>
         /// 构造函数 创建缓存池管理者对象结点，预制体缓存池结点
         /// </summary>
         /// <param name="obj">缓存池物体</param>
-        /// <param name="poolMgrObj">缓存池管理游戏对象</param>
-        public PoolData(GameObject obj, GameObject poolMgrObj)
+        public PoolData(GameObject obj)
         {
             // 创建父节点物体
             this.parentObj = new GameObject(obj.name + " Pool");
             // 把父节点物体作为缓存池管理对象的子节点
-            this.parentObj.transform.SetParent(poolMgrObj.transform, false);
-            // 初始化缓存池
-            poolList = new List<GameObject>(8);
-            // 把物体压入缓存池
-            RealeaseObj(obj);
+            //this.parentObj.transform.SetParent(poolMgr.transform, false);
+
+            // 把物体压入已使用记录中
+            PushUsedList(obj);
         }
 
+        /// <summary>
+        /// 从缓存池中取出对象
+        /// </summary>
+        /// <returns></returns>
         public GameObject GetObj()
         {
             GameObject obj = null;
 
-            // 取出缓存池中最后一个对象
-            obj = poolList[^1];
-            // 从缓存池中移除最后一个对象
-            poolList.RemoveAt(poolList.Count - 1);
+            if (Count > 0)
+            {
+                // 取出缓存池中一个对象
+                obj = dataStack.Pop();
+                // 在已经使用容器中记录这个对象
+                usedList.Add(obj);
+            }
+            else
+            {
+                // 从已经使用的队列中取出最久没有使用的物体
+                obj = usedList[0];
+                usedList.RemoveAt(0);
+                // 再次入队
+                usedList.Add(obj);
+            }
 
+            // 激活对象
             obj.SetActive(true);
+            // 断开物体和缓存池的父子关系
             obj.transform.parent = null;
-
-            // 重置最后一次使用时间
-            lastUseTime = Time.time;
 
             return obj;
         }
 
         /// <summary>
-        /// 压入缓存池
+        /// 把物体压入缓存池
         /// </summary>
         /// <param name="obj"></param>
         public void RealeaseObj(GameObject obj)
         {
-            poolList.Add(obj);
-            obj.transform.SetParent(parentObj.transform, false);
             obj.SetActive(false);
+            obj.transform.SetParent(parentObj.transform, false);
 
-            // 重置最后一次使用时间
-            lastUseTime = Time.time;
+            // 压入栈中
+            dataStack.Push(obj);
+            // 删除已使用记录
+            usedList.Remove(obj);
         }
 
-        public bool InUnUsed(float currentTime)
+        public void PushUsedList(GameObject obj)
         {
-            //Debug.Log(currentTime - lastUseTime);
-            return currentTime - lastUseTime >= clearUpInterval;
+            usedList.Add(obj);
         }
 
+        /// <summary>
+        /// 清空缓存池
+        /// </summary>
         public void Clear()
         {
-            foreach (GameObject obj in poolList)
-            {
-                GameObject.Destroy(obj);
-            }
+            dataStack.Clear();
+            usedList.Clear();
             GameObject.Destroy(parentObj);
-            poolList.Clear();
             parentObj = null;
         }
     }
@@ -86,38 +98,42 @@ namespace GameFramework.ObjectPoolManager
     /// <summary>
     /// 缓存池管理器
     /// </summary>
-    public class PoolMgr : SingletonAutoMono<PoolMgr>
+    public class PoolMgr : Singleton<PoolMgr>
     {
         /// <summary>
         /// 缓存池容器 键：某一类物品名字，值：游戏对象
         /// </summary>
-        public Dictionary<string, PoolData> poolDic = new Dictionary<string, PoolData>();
-
-        private void Update()
-        {
-            // 定期清理缓存池
-            CleanupUnusedPools();
-        }
+        private Dictionary<string, PoolData> poolDic = new Dictionary<string, PoolData>();
 
         /// <summary>
         /// 从缓存池中取物体
         /// </summary>
         /// <param name="name">物体名字</param>
         /// <param name="callback">回调函数</param>
-        public void GetObj(string name, UnityAction<GameObject> callback = null, string path = "Prefabs/")
+        public void GetObj(string name, UnityAction<GameObject> callback = null, string path = "Prefabs/", int maxNum = 50)
         {
-            // 判断对应的对象池是否存在，且池子里有物体
-            if (poolDic.ContainsKey(name) && poolDic[name].poolList.Count > 0)
-            {
-                callback?.Invoke(poolDic[name].GetObj());
-            }
-            else
+            // 判断对应的对象池是否存在
+            if (!poolDic.ContainsKey(name) ||
+                (poolDic[name].Count == 0 && poolDic[name].UseCount < maxNum))
             {
                 // 异步加载预制体资源
                 ResourcesMgr.Instance.LoadResAsync<GameObject>(path + name, (resObj) =>
                 {
                     callback?.Invoke(resObj);
+
+                    if (!poolDic.ContainsKey(name))
+                    {
+                        poolDic.Add(name, new PoolData(resObj));
+                    }
+                    else
+                    {
+                        poolDic[name].PushUsedList(resObj);
+                    }
                 }, name);
+            }
+            else
+            {
+                callback?.Invoke(poolDic[name].GetObj());
             }
         }
 
@@ -134,41 +150,12 @@ namespace GameFramework.ObjectPoolManager
             }
             else
             {
-                poolDic.Add(name, new PoolData(obj, Instance.gameObject));
+                poolDic.Add(name, new PoolData(obj));
             }
         }
 
         /// <summary>
-        /// 定期清理缓存池
-        /// </summary>
-        public void CleanupUnusedPools()
-        {
-            // 记录当前时间
-            float currentTime = Time.time;
-            // 存储待移除键名
-            List<string> poolsToRemove = new List<string>();
-
-            // 循环缓存池容器里的所有缓存池对象
-            foreach (KeyValuePair<string, PoolData> kv in poolDic)
-            {
-                // 判断是否长时间未使用
-                if (kv.Value.InUnUsed(currentTime))
-                {
-                    //poolDic[kv.Key].Clear();
-                    // 协程清理超时对象池资源
-                    StartCoroutine(ClearUpPool(poolDic[kv.Key]));
-                    poolsToRemove.Add(kv.Key);
-                }
-            }
-            // 删除已经
-            foreach (string poolName in poolsToRemove)
-            {
-                poolDic.Remove(poolName);
-            }
-        }
-
-        /// <summary>
-        /// 清空缓存池
+        /// 清空所有的缓存池
         /// </summary>
         public void Clear()
         {
@@ -179,15 +166,11 @@ namespace GameFramework.ObjectPoolManager
             poolDic.Clear();
         }
 
-        /// <summary>
-        /// 清除缓存协程
-        /// </summary>
-        /// <param name="poolData"></param>
-        /// <returns></returns>
-        private IEnumerator ClearUpPool(PoolData poolData)
+        public override void Dispose()
         {
-            poolData.Clear();
-            yield return null;
+            if (IsDisposed) return;
+            Clear();
+            base.Dispose();
         }
     }
 }
