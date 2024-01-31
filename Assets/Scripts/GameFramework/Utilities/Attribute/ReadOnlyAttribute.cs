e40eba0b23d6053f73b0129c300590b141d800d5
虫儿努力向上飞 <55835505+AzureBubble@users.noreply.github.com>
2024-01-31 14:41:26 +0800

    修改只读特性

--- Assets/Scripts/GameFramework/Utilities/Attribute/ReadOnlyAttribute.cs
+++ Assets/Scripts/GameFramework/Utilities/Attribute/ReadOnlyAttribute.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class ReadOnlyAttribute : PropertyAttribute
 {
+    public bool isReadOnly = true;
 }
 
 #if UNITY_EDITOR
@@ -15,10 +16,19 @@ public class ReadOnlyDrawer : PropertyDrawer
         return EditorGUI.GetPropertyHeight(property, label, true);
     }
 
+    //public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    //{
+    //    GUI.enabled = false;
+    //    EditorGUI.PropertyField(position, property, label, true);
+    //    GUI.enabled = true;
+    //}
+
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label, true);
+        ReadOnlyAttribute readOnlyAttribute = (ReadOnlyAttribute)attribute;
+        readOnlyAttribute.isReadOnly = EditorGUI.Toggle(new Rect(position.x + position.width - 20, position.y, 20, position.height), readOnlyAttribute.isReadOnly);
+        GUI.enabled = !readOnlyAttribute.isReadOnly;
+        EditorGUI.PropertyField(new Rect(position.x, position.y, position.width - 20, position.height), property, label, true);
         GUI.enabled = true;
     }
 }
